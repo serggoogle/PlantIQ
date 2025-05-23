2230b5c5fbe6e6b9b2fe53f8b8cbdfa1cc7f9dec
Sergio Olvera <solvera1921@gmail.com>
2025-05-22 21:52:41 -0600

    Created send_payload method

--- lib/MQTT/MQTT.h
+++ lib/MQTT/MQTT.h
@@ -7,8 +7,9 @@ public:
     MQTT();
     MQTT(const char* mqtt_server, int mqtt_port);
     void mqtt_connect();
+    static void send_payload(String name, String sensor, float value);
 private:
-    const char* _mqtt_broker;
+    const char *_mqtt_broker;
     int _mqtt_port;
     PubSubClient *mqttClient;
 };
