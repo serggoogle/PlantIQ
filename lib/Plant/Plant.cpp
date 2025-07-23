8f974e5f8e406f9e2d6a34249a58b718b125afa3
Sergio Olvera <solvera1921@gmail.com>
2025-07-22 23:59:36 -0600

    Configured Moisture Sensor

--- lib/Plant/Plant.cpp
+++ lib/Plant/Plant.cpp
@@ -1,22 +1,27 @@
 #include "Arduino.h"
 #include "Plant.h"
 
-Plant::Plant(const String &name)
+Plant::Plant(const char* name)
 {
     _name = name;
 }
 
-Plant::Plant(const String &name, const String &species)
+Plant::Plant(const char* name, const char* species)
 {
     _name = name;
     _species = species;
+    _label = "{job=\"" + _name + "\",species=\"" + _species + "\"}";
 }
 
-String Plant::getName()
+const char* Plant::getName()
 {
-    return _name;
+    return _name.c_str();
 }
 
-String Plant::getSpecies() {
-    return _species;
+const char* Plant::getSpecies() {
+    return _species.c_str();
+}
+
+const char* Plant::getLabel(){
+    return _label.c_str();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,108 @@
 #include <Arduino.h>
 #include <PrometheusArduino.h>
 #include "Plant.h"
-#include "Temperature.h"
 #include "Network.h"
+#include "Adafruit_SHT31.h"
 #include "Prometheus.h"
+#include "Moisture.h"
 #include "MQTT.h"
 
 using namespace std;
 
-#define BATCH_SIZE 5
-#define NUM_SERIES 2
-#define BUFFER_SIZE 512
+constexpr int8_t SHT31_ADDR = 0x44; // Set to 0x45 for alternate i2c addr
+constexpr size_t BATCH_SIZE = 5;
+constexpr size_t NUM_SERIES = 4;
+constexpr size_t BUFFER_SIZE = 1024;
+constexpr const char* PLANT_NAME = "test-plant-1";
+constexpr const char* PLANT_SPECIES = "TestSpecies";
+constexpr float DEFAULT_SENSOR_VALUE = 0.0f;
+constexpr bool DEBUG = true;
 
-Plant plant("test-plant-1", "TestSpecies");
-String labels = "{job=\"" + plant.getName() + "\",host=\"esp32\"}";
-TimeSeries ts1(BATCH_SIZE, "plant_temperature_f", labels.c_str());
-TimeSeries ts2(BATCH_SIZE, "plant_moisture", labels.c_str());
-Prometheus prometheus(NUM_SERIES,BUFFER_SIZE);
+Adafruit_SHT31 SHT31_SENSOR = Adafruit_SHT31();
+Plant PLANT(PLANT_NAME, PLANT_SPECIES);
+const char * LABEL = PLANT.getLabel();
+Prometheus PROMETHEUS(NUM_SERIES,BUFFER_SIZE);
+TimeSeries TEMPERATURE_C_TS(BATCH_SIZE, "temperature_c", LABEL);
+TimeSeries TEMPERATURE_F_TS(BATCH_SIZE, "temperature_f", LABEL);
+TimeSeries HUMIDITY_TS(BATCH_SIZE, "humidity", LABEL);
+TimeSeries MOISTURE_TS(BATCH_SIZE, "moisture", LABEL);
+
 
 int loopCounter = 0;
+bool enableHeater = false;
 
 void setup() {
-    delay(3500);
     Serial.begin(9600);
+    while (!Serial) {
+        delay(10); 
+    }
     Serial.println("*********************************************************");
     Serial.println("> Plant Details: ");
-    Serial.printf("Plant Name: %s\n", plant.getName().c_str());
-    Serial.printf("Plant Species: %s\n", plant.getSpecies().c_str());
+    Serial.printf("Plant Name: %s\n", PLANT.getName());
+    Serial.printf("Plant Species: %s\n", PLANT.getSpecies());
+
+    Serial.println("> Initializing SHT31 Temperature/Moisture Sensor:");
+    if (!SHT31_SENSOR.begin(SHT31_ADDR)) {
+        Serial.println("Couldn't find SHT31");
+        while(1) delay(1);
+    }
+
     Serial.println("> Instantiating Prometheus:");
-    prometheus.setupClients(plant.getName().c_str(), true);
-    prometheus.addTimeSeries(ts1);
-    prometheus.addTimeSeries(ts2); // should error out if using default Prometheus constructor with numSeries=1
-    Serial.printf("Prometheus Labels: %s\n", labels.c_str());
+    Serial.printf("Prometheus Labels: %s\n",LABEL);
+    PROMETHEUS.setupClients(PLANT.getName(), DEBUG);
+    PROMETHEUS.addTimeSeries(TEMPERATURE_C_TS);
+    PROMETHEUS.addTimeSeries(TEMPERATURE_F_TS);
+    PROMETHEUS.addTimeSeries(HUMIDITY_TS);
+    PROMETHEUS.addTimeSeries(MOISTURE_TS);
     Serial.println("*********************************************************\n");
 }
+
 
 void loop() {
     /*
      *  Periodically sends data to remote prometheus and mqtt servers
      */
-    int64_t time = prometheus.getTimeMillis();
-    float temperature_f = 69;
-    float moisture = 500;
+    float temperature_c, temperature_f, humidity = DEFAULT_SENSOR_VALUE;
+    uint16_t moisture = Moisture::getMoisture();
+    int64_t time = PROMETHEUS.getTimeMillis();
+    if (SHT31_SENSOR.readBoth(&temperature_c, &humidity)){
+        temperature_f = (temperature_c * 9.0 / 5.0) + 32.0;
+        if (!TEMPERATURE_C_TS.addSample(time, temperature_c)) {
+            Serial.println(TEMPERATURE_C_TS.errmsg);
+        }
+        if (!TEMPERATURE_F_TS.addSample(time, temperature_f)) {
+            Serial.println(TEMPERATURE_F_TS.errmsg);
+        }
+        if(!HUMIDITY_TS.addSample(time, humidity)){
+            Serial.println(HUMIDITY_TS.errmsg);
+        }
+    }
+    else{
+        Serial.println("Error reading temperature & humidity");
+    }
+    if (moisture < 0) {
+        Serial.println("Error reading moisture");
+        moisture = DEFAULT_SENSOR_VALUE; // Set to default value on error
+    } else {
+        Serial.printf("Moisture: %d\n", moisture);
+        if (!MOISTURE_TS.addSample(time, moisture)) {
+            Serial.println(MOISTURE_TS.errmsg);
+        }
+    }
 
-    if (loopCounter < BATCH_SIZE) {
-        if (!ts1.addSample(time, temperature_f)) {
-            Serial.println(ts1.errmsg);
+    if (loopCounter >= BATCH_SIZE) {
+        //Send after batch is full
+        PromClient::SendResult res = PROMETHEUS.client.send(PROMETHEUS.req);
+        if (!res == PromClient::SendResult::SUCCESS) {
+            Serial.printf("Error: Unable to send request(%s)\n", PROMETHEUS.client.errmsg);
         }
-        if (!ts2.addSample(time, moisture)) {
-            Serial.println(ts2.errmsg);
-        }
-        loopCounter++;
+        // Reset batches after a successful send.
+        TEMPERATURE_C_TS.resetSamples();
+        TEMPERATURE_F_TS.resetSamples();
+        HUMIDITY_TS.resetSamples();
+        MOISTURE_TS.resetSamples();
+        loopCounter = 0;
     }
-    //Send after batch is full
-    PromClient::SendResult res = prometheus.client.send(prometheus.req);
-    if (!res == PromClient::SendResult::SUCCESS) {
-        Serial.printf("Error: Unable to send request(%s)\n", prometheus.client.errmsg);
-    }
-    // Reset batches after a successful send.
-    ts1.resetSamples();
-    ts2.resetSamples();
-    loopCounter = 0;
-    delay(500);
+    loopCounter++;
+    delay(1000);
 }
