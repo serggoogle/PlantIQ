bda2bb1ba498960dd8ee1b9f06cc8a2959f40ebb
Sergio Olvera <solvera1921@gmail.com>
2025-12-29 00:11:05 -0600

    Added datasource creation and dashboard import during setup for grafana

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+GF_DEFAULT_USER_PASS="admin:admin"
+create_prometheus_ds(){
+    NUM_OF_PROM_DS=$(curl -u $GF_DEFAULT_USER_PASS -sS 'http://localhost:3000/api/datasources' | jq '.[].name ' | grep prometheus | wc -l)
+    if [ $NUM_OF_PROM_DS -eq 0 ]; then
+        echo "Creating Prometheus Datasource..."
+        POST_MESSAGE=$(curl -u $GF_DEFAULT_USER_PASS -sS -X POST http://localhost:3000/api/datasources \
+        -H 'content-type: application/json' \
+        -d @infrastructure/grafana/Prometheus-Datasource-Template.json | jq -r .message)
+        echo "> $POST_MESSAGE"
+        sleep 3
+    fi
+}
+
+import_metrics_dashboard(){
+    echo "Importing Dashboard..."
+    DASHBOARD_IMPORTED=$(curl -u $GF_DEFAULT_USER_PASS -sS -X POST http://localhost:3000/api/dashboards/import \
+    -H 'accept: application/json' -H 'Content-Type: application/json' \
+    -d @infrastructure/grafana/Plant-Metrics-Template.json | jq '.imported')
+    if [ "$DASHBOARD_IMPORTED" = "true" ]; then
+        echo "> Dashboard successfully imported"
+    fi
+}
 
 setup_dev_environment() {
     echo "Building development environment..."
@@ -13,17 +35,27 @@ setup_dev_environment() {
     fi
 }
 
+setup_grafana(){
+    echo "Setting up Grafana..."
+    sleep 2
+    create_prometheus_ds
+    import_metrics_dashboard
+}
+
 setup_infrastructure() {
     echo "Setting up metrics infrastructure..."
-    docker-compose -f infrastructure/metrics-compose.yml -f infrastructure/flink-compose.yml up -d
+    docker-compose -f infrastructure/metrics-compose.yml -f infrastructure/flink-compose.yml up -d --wait
     if [ $? -ne 0 ]; then
         echo "Failed to build infrastructure services."
         exit 1
     fi
+    echo "All Services are up and running"
 }
 
 if [[ -z $(docker ps --filter name=dev-container --format={{.ID}}) ]]; then
 	setup_dev_environment && echo "Development container successfuly created" 
 fi
 
-setup_infrastructure && echo "All services are up and running."
+setup_infrastructure
+setup_grafana
+echo "Done"
