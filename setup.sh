a4edfb35d639d650ead80e26bbed8763241f775d
Sergio Olvera <solvera533@gmail.com>
2026-01-05 10:20:30 -0600

    Update infrastructure (#20)
    * Added datasource creation and dashboard import during setup for grafana
    
    * Updated setup script
    
    * Updated flink and metric services
    
    * Updated paths and added prometheus config
    
    * Created base infrastructure for Rabbitmq

--- setup.sh
+++ setup.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+GF_DEFAULT_USER_PASS="admin:admin"
+create_prometheus_ds(){
+    NUM_OF_PROM_DS=$(curl -u $GF_DEFAULT_USER_PASS -sS 'http://localhost:3000/api/datasources' | jq '.[].name ' | grep prometheus | wc -l)
+    if [ $NUM_OF_PROM_DS -eq 0 ]; then
+        echo "Creating Prometheus Datasource..."
+        POST_MESSAGE=$(curl -u $GF_DEFAULT_USER_PASS -sS -X POST http://localhost:3000/api/datasources \
+        -H 'content-type: application/json' \
+        -d @infrastructure/grafana/datasources/Prometheus-Datasource.json | jq -r .message)
+        echo "> $POST_MESSAGE"
+        sleep 3
+    fi
+}
+
+import_dashboards(){
+    echo "Importing Dashboards..."
+    dashboards=($(ls infrastructure/grafana/dashboards))
+    for dashboard in "${dashboards[@]}"; do
+        DASHBOARD_IMPORTED=$(curl -u $GF_DEFAULT_USER_PASS -sS -X POST http://localhost:3000/api/dashboards/import \
+        -H 'accept: application/json' -H 'Content-Type: application/json' \
+        -d @infrastructure/grafana/dashboards/$dashboard | jq '.imported')
+        if [ "$DASHBOARD_IMPORTED" = "true" ]; then
+            echo "> $dashboard dashboard successfully imported"
+        fi
+        sleep 1
+    done
+}
 
 setup_dev_environment() {
     echo "Building development environment..."
@@ -13,17 +39,31 @@ setup_dev_environment() {
     fi
 }
 
+setup_grafana(){
+    echo "Setting up Grafana..."
+    sleep 3
+    create_prometheus_ds
+    import_dashboards
+}
+
 setup_infrastructure() {
     echo "Setting up metrics infrastructure..."
-    docker-compose -f infrastructure/metrics-compose.yml -f infrastructure/flink-compose.yml up -d
-    if [ $? -ne 0 ]; then
-        echo "Failed to build infrastructure services."
-        exit 1
-    fi
+    composeFiles=($(ls infrastructure/*.yml))
+
+    for file in "${composeFiles[@]}"; do
+        docker-compose -f $file up -d --wait
+        if [ $? -ne 0 ]; then
+            echo "Failed to build $file infrastructure services."
+            exit 1
+        fi
+    done
+    echo "All Services are up and running"
 }
 
 if [[ -z $(docker ps --filter name=dev-container --format={{.ID}}) ]]; then
 	setup_dev_environment && echo "Development container successfuly created" 
 fi
 
-setup_infrastructure && echo "All services are up and running."
+setup_infrastructure
+setup_grafana
+echo "Done"
--- teardown.sh
+++ teardown.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 composeFiles=($(find infrastructure/ -name "*-compose.yml"))
+volumes=($(docker volume ls | grep infrastructure_ | awk '{print $2}'))
+
+delete_infrastructure_volumes(){
+	for volume in "${volumes[@]}"; do
+		docker volume rm $volume > /dev/null && echo "$volume deleted"
+	done
+}
 
 for file in "${composeFiles[@]}"; do
 	read -p "Teardown $file?[y/n]: " -n 1 -r
 	[[ ! -z "$REPLY" ]] && echo
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		docker compose -f $file down
 	fi
 done
 
+read -p "Remove infrastructure volumes?[y/n]: " -n 1 -r 
+[[ ! -z "$REPLY" ]] && echo 
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	delete_infrastructure_volumes
+fi
+
 read -p "Teardown development environment?[y/n]: " -n 1 -r
 [[ ! -z "$REPLY" ]] && echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     if [ "$(docker ps -a -q -f name=dev-container)" ]; then
     	docker stop dev-container > /dev/null  && docker rm dev-container > /dev/null
 		echo "Removed dev container."
